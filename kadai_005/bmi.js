e36082030ae43b41c8add360b54103dc323563bd
ksghnhmwww <kiwamu1997@gmail.com>
2026-04-30 22:19:00 +0900

    kadai_005をアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+// 変数の宣言(身長)
+let height;
+
+// 値の代入(身長(m))
+height = 1.7;
+
+// 変数の宣言(体重)
+let weight;
+
+// 値の代入(体重(kg))
+weight = 68;
+
+// 定数の宣言(BMI)
+const bmi = weight / (height * height);
+
+// コンソールへの出力
+console.log(bmi);
