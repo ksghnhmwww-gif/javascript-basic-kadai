83b27529435f5bd87f45f0e91702fe570230ab97
ksghnhmwww <kiwamu1997@gmail.com>
2026-04-30 22:42:07 +0900

    kadai_005を修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -10,8 +10,8 @@ let weight;
 // 値の代入(体重(kg))
 weight = 68;
 
-// 定数の宣言(BMI)
-const bmi = weight / (height * height);
+// 変数の宣言(BMI)
+let bmi = weight / (height * height);
 
 // コンソールへの出力
 console.log(bmi);
