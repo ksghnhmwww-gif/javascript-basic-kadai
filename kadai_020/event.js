b7eefbb15f22fdc18027776954dc6b221a27b971
ksghnhmwww <kiwamu1997@gmail.com>
2026-05-05 01:05:48 +0900

    kadai_020をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  // textというidを持つHTML要素を取得し、定数に代入する
+  const text = document.getElementById('text');
+  // 文字変更を実施する
+  text.textContent = 'ボタンをクリックしました'
+})
